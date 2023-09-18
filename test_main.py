3977c30998fc9a8ae1f7702ef745bdeb5b36608d
suim.park <suim.park@duke.edu>
2023-09-18 03:34:54 -0400

    Edit Files

--- test_main.py
+++ test_main.py
@@ -4,11 +4,11 @@ from main import calculate_stat, build_histogram
 
 
 def test_calculate_stat():
-    calculate_stat()
+    assert calculate_stat()
 
 
 def test_hist():
-    build_histogram()
+    assert build_histogram()
 
 
 if __name__ == "__main__":
