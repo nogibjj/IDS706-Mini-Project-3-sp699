5a68a322f26ac2b486d18123abff30a1489f7e2a
suim.park <suim.park@duke.edu>
2023-09-18 03:59:57 -0400

    Edit test_main.py file

--- test_main.py
+++ test_main.py
@@ -1,12 +1,12 @@
 # Test main.py
 
 import polars as pl
-from main import load_data, calculate_stat, build_histogram
+from main import calculate_stat, build_histogram
 
 
 def test_calculate_stat():
-    result1 = load_data["flipper_length_mm"].median()
-    assert result1 == 197.0
+    result1 = calculate_stat()
+    assert result1 is not None
 
 
 def test_hist():
